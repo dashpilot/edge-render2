56aad3a3adf36f5e7ce7e0a030fac34235c84251
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-05-17 23:16:21 +0000

    Actually render different pages

--- api/render.js
+++ api/render.js
@@ -1,4 +1,4 @@
-import { template } from "../modules/category.js";
+import { render } from "../modules/category.js";
 
 export const config = {
   runtime: "edge",
@@ -8,7 +8,7 @@ export default async function handler(req) {
   const { searchParams } = new URL(req.url);
   const category = searchParams.get("category");
 
-  const r = await fetch("/data.json");
+  const r = await fetch("https://edge-render2.vercel.app/data.json");
   var data = await r.json();
   data.category = category;
 
--- modules/category.js
+++ modules/category.js
@@ -2,11 +2,79 @@ import { ifEq, forLoop } from "./helpers.js";
 
 function render(data) {
   return `
-  
-  		<h1>${data.category}</h1>
-
-  		${forLoop(data.posts, "<h1>${title}</h1><p>${body}</p>")}
+  		${header()}
+		
+		<div class="container mt-5">
+		
+  			<h1 class="mb-5">${data.category}</h1>
 	
+  			${forLoop(
+          data.posts.filter((x) => x.category == data.category),
+          "<h2>${title}</h2><p>${body}</p>"
+        )}
+		  
+		</div>
+		  
+		${footer()}
+	
+	`;
+}
+
+function header() {
+  return `<!DOCTYPE html>
+	<html>
+	  <head>
+		<meta charset="UTF-8" />
+		<title>Vercel Edge</title>
+		<link
+		  href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+		  rel="stylesheet"
+		  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+		  crossorigin="anonymous"
+		/>
+	
+		<style>
+		  section {
+			padding: 40px;
+		  }
+	
+		  nav a {
+			color: white;
+			text-decoration: none;
+		  }
+		</style>
+	
+		<script type="module">
+		  import { render } from "./modules/category.js";
+	
+		  const r = await fetch("data.json");
+		  const data = await r.json();
+	
+		  var result = render(data);
+	
+		  //var navTpl = nav();
+	
+		  // document.querySelector("#nav").innerHTML = navTpl;
+		  document.querySelector("#page").innerHTML = result;
+		</script>
+	  </head>
+	
+	  <body>
+		<div id="nav">
+		  <nav class="p-3 text-light bg-dark">
+			<a href="/" class="ms-3">Home</a>
+			<a href="/c/about" class="ms-3">About</a>
+			<a href="/c/contact" class="ms-3">Contact</a>
+		  </nav>
+		</div>
+		`;
+}
+
+function footer() {
+  return `
+	<div id="app"></div>
+	  </body>
+	</html>
 	`;
 }
 
