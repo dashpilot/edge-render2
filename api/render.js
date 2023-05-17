15732c0ee15de7c496ea475eea1a5addba0ac129
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2023-05-17 22:58:22 +0000

    tweak

---
+++ api/render.js
@@ -0,0 +1,23 @@
+import { template } from "../modules/category.js";
+
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler(req) {
+  const { searchParams } = new URL(req.url);
+  const category = searchParams.get("category");
+
+  const r = await fetch("/data.json");
+  var data = await r.json();
+  data.category = category;
+
+  var result = render(data);
+
+  return new Response(result, {
+    status: 200,
+    headers: {
+      "content-type": "text/html",
+    },
+  });
+}
--- modules/category.js
+++ modules/category.js
@@ -2,6 +2,8 @@ import { ifEq, forLoop } from "./helpers.js";
 
 function render(data) {
   return `
+  
+  		<h1>${data.category}</h1>
 
   		${forLoop(data.posts, "<h1>${title}</h1><p>${body}</p>")}
 	
